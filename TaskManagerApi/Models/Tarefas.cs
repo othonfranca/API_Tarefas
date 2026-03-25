3980b824a9228cbb2c114f74a08887d1d9a002e9
Othon Franca <othon.franca.dev@gmail.com>
2026-03-25 14:42:32 -0300

    Pacotes instalados, pastas criadas e modelos criados

---
+++ TaskManagerApi/Models/Tarefas.cs
@@ -0,0 +1,14 @@
+using TaskManagerApi.Enum;
+
+namespace TaskManagerApi.Models;
+
+public class Tarefa
+{
+    public int Id {get;set;}
+    public DateTime DataCriacao {get;set;} = DateTime.Now;
+    public string Titulo {get;set;} = string.Empty;
+    public string Descricao {get;set;} = string.Empty;
+    public StatusTarefa Status {get;set;} = StatusTarefa.Pendente;
+    public int ColaboradorId {get;set;} // chave estrangeira para Colaborador
+    public Colaborador? Colaborador {get;set;} // propriedade de navegação para Colaborador
+}
